457edb151bc51ed6a65fe589ebc369f729daf844
YOUR NAME <will19940625@gmail.com>
2015-11-12 03:32:11 +0800

    Import Project contain to Demo
    import

---
+++ AutoLayoutDemo/AutoLayout_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout_Demo
+//
+//  Created by Tkuimb310Applymini06 on 2015/11/10.
+//  Copyright © 2015年 Tkuimb310Applymini06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
